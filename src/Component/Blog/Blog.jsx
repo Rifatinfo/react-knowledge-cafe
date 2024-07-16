4153b9af4a7c0868a2fbfad947b9335a5da0edae
Rifatinfo <mdrifathossainsinfo@gmail.com>
2024-07-16 15:34:19 +0600

    all post element show

--- src/Component/Blog/Blog.jsx
+++ src/Component/Blog/Blog.jsx
@@ -2,9 +2,28 @@ import PropTypes from 'prop-types';
 
 const Blog = ({blog}) => {
     console.log(blog);
+    const {title, cover, author_name, author_img, posted_date, reading_time, hashtags} = blog;
     return (
         <div>
-            
+            <img src={cover} alt={`Cover picture of the title ${title}`} />
+            <div className='flex justify-between'>
+                <div className='flex'>
+                   <img className='w-14 rounded-full' src={author_img} alt="" />
+                   <div className='ml-6'>
+                    <h3 className='text-2xl'>{author_name}</h3>
+                    <p>{posted_date}</p>
+                   </div>
+                </div>
+                <div>
+                   <span>{reading_time} read</span>
+                </div>
+            </div>
+            <h2 className='text-4xl'>{title}</h2>
+            <p>
+                {
+                    hashtags.map((hash, idx) => <span key={idx}><a href="">#{hash}</a></span>)
+                }
+            </p>
         </div>
     );
 };
