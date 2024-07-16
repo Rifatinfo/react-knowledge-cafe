4afcf5a2c3ad7fe64b63d83f853f30f3f5e115dc
Rifatinfo <mdrifathossainsinfo@gmail.com>
2024-07-16 14:21:48 +0600

    header, blog and PropTypes create

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css'
 import Blogs from './Component/Blogs/Blogs'
+import Bookmarkes from './Component/Bookmarks/Bookmarkes'
 import Header from './Component/Header/Header'
 function App() {
  
   return (
     <>
       <Header></Header>
-      <Blogs></Blogs>
+      <main className='md:flex'>
+        <Blogs></Blogs>
+        <Bookmarkes></Bookmarkes>
+      </main>
     </>
   )
 }
